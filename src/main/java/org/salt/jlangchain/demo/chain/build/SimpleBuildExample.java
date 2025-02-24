aa374384b21b539639af786feb6dd7d85cec9c06
huaqianshu_z <huaqianshu_z@163.com>
2025-02-24 21:58:13 +0800

    first commit

--- src/main/java/org/salt/jlangchain/demo/chain/build/SimpleBuildExample.java
+++ src/main/java/org/salt/jlangchain/demo/chain/build/SimpleBuildExample.java
@@ -14,28 +14,17 @@
 
 package org.salt.jlangchain.demo.chain.build;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.salt.function.flow.FlowInstance;
-import org.salt.function.flow.Info;
-import org.salt.function.flow.context.ContextBus;
-import org.salt.function.flow.node.IResult;
 import org.salt.jlangchain.core.BaseRunnable;
 import org.salt.jlangchain.core.ChainActor;
-import org.salt.jlangchain.core.llm.ollama.ChatOllama;
 import org.salt.jlangchain.core.llm.openai.ChatOpenAI;
-import org.salt.jlangchain.core.message.AIMessage;
 import org.salt.jlangchain.core.parser.StrOutputParser;
 import org.salt.jlangchain.core.parser.generation.ChatGeneration;
-import org.salt.jlangchain.core.parser.generation.Generation;
-import org.salt.jlangchain.core.prompt.chat.ChatPromptTemplate;
 import org.salt.jlangchain.core.prompt.string.PromptTemplate;
-import org.salt.jlangchain.core.prompt.value.ChatPromptValue;
 import org.salt.jlangchain.core.prompt.value.StringPromptValue;
-import org.salt.jlangchain.utils.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Map;
 
 @Component
